47b9b2c9e4d7b334daa90535dc50baa4a6abf25b
modesty0310 <rlatprua4@gmail.com>
2024-03-22 16:10:12 +0900

    feat: json to model

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter_toonflix/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = 'today';
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonList = [];
     final url = Uri.parse('$baseUrl/$today');
     final res = await http.get(url);
-    print(res.body);
+    if (res.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(res.body);
+      for (var webtoon in webtoons) {
+        webtoonList.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonList;
+    }
+    throw Error();
   }
 }
